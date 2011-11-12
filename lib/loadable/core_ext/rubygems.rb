ed40e663c66291a35043e2aec889a2f572fbce55
7rans <transfire@gmail.com>
2011-11-11 19:34:19 -0500

    Fix argument name in rubygems search. [bug]

--- lib/loadable/core_ext/rubygems.rb
+++ lib/loadable/core_ext/rubygems.rb
@@ -1,12 +1,7 @@
 module Gem
 
-  # TODO: Below are two implementations of the same feature. Currently
-  # we are using `Gem.search` code which calls `Gem::Specification.current_specs`.
-  # Possibly this could be replaced by simply calling `GemPathSearcher.current_files`.
-  # Hoverver, it is unclear to me at this time which is the best course of action.
-
   # Search RubyGems for matching paths in current gem versions.
-  def self.search(fname, options={})
+  def self.search(match, options={})
     return unless defined?(::Gem)
     matches = []
     Gem::Specification.current_specs.each do |spec|
